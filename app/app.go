177d2c5ba9ca71ff78789a4a5fbfca5f4655584e
Tabuci Octavian <octavian@tabuci.org>
2023-04-11 14:20:51 +0300

    Added new feature - on soft delete, update other fields from model in same update operation

--- app/app.go
+++ app/app.go
@@ -5,54 +5,39 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
-	"time"
 )
 
 const postgresDSN = "user=root password= dbname=gorm host=localhost port=26257 sslmode=disable TimeZone=Europe/Chisinau"
 
 type User struct {
-	ID       uuid.UUID
-	UserName string
+	ID uuid.UUID `gorm:"primaryKey;type:uuid;not null;->;default:gen_random_uuid()"`
 
-	UpdatedAt   *time.Time
-	DeletedByID *uuid.UUID `gorm:"updateOnSoftDelete"`
+	DeletedByID *uuid.UUID `gorm:"type:uuid;updateOnSoftDelete"`
 	DeletedAt   *gorm.DeletedAt
 }
 
 func main() {
-	log.Println("hello world")
 	db, err := gorm.Open(postgres.Open(postgresDSN), &gorm.Config{})
-
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	//var users []User
-	//result := db.Find(&users)
-	//if result.Error != nil {
-	//	log.Fatalln(result.Error)
-	//}
-	//
-	//log.Println(users)
-
 	var id = "60ea2d8f-4fac-42ba-b363-4536f6a8edf4"
 
+	// Retrieve the user
 	var user User
 	result := db.First(&user, "id = ?", id)
 	if result.Error != nil {
 		log.Fatalln(result.Error)
 	}
 
+	// Set the user who will delete the record
 	deletedByID := uuid.New()
 	user.DeletedByID = &deletedByID
 
-	log.Println(user)
-
-	log.Println("DELETING......")
+	// Delete
 	result = db.Delete(&user)
 	if result.Error != nil {
 		log.Fatalln(result.Error)
 	}
-	//log.Println("RESTORING...")
-	//db.Model(&User{}).Unscoped().Where("id = ?", id).Update("deleted_at", nil)
 }
